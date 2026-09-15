c04d9521706872ae80979c76c5e23175090f51be
jskeen <jskeen@Jons-Laptop.localdomain>
2026-09-15 13:13:24 -0400

    added buy button and fixed text sizes

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ document.querySelectorAll("[data-year]").forEach((el) => {
   el.textContent = year;
 });
 
-const plotted = document.querySelector("[data-plotted]");
-if (plotted) {
-  plotted.textContent = new Intl.DateTimeFormat("en-GB", {
-    year: "numeric",
-    month: "short",
-    day: "2-digit",
-  })
-    .format(new Date())
-    .toUpperCase();
-}
+const plotted = document.querySelectorAll("[data-plotted]");
+const plottedValue = new Intl.DateTimeFormat("en-GB", {
+  year: "numeric",
+  month: "short",
+  day: "2-digit",
+})
+  .format(new Date())
+  .toUpperCase();
+plotted.forEach((el) => {
+  el.textContent = plottedValue;
+});
